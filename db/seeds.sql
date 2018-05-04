5a635e9cd229014a1c0ad715fb50093a0ebd00eb
Rovch <wesleyjacksonjr33@hotmail.com>
2018-05-03 22:18:17 -0500

    done

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cool burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon burger');
